cf42eb7ffaf90172fc49a0cf218414b272a34cbc
Daphne Avril <daphneavril@outlook.com>
2022-06-27 22:06:07 +0200

    merge

--- js/examples/examples.portfolio.js
+++ js/examples/examples.portfolio.js
@@ -103,7 +103,7 @@ Theme Version:	7.5.0
 			stopAfterLoops: 0,
 			stopAtSlide: -1,
 			shuffle: "off",
-			autoHeight: "off",
+			autoHeight: "on",
 			disableProgressBar: "on",
 			hideThumbsOnMobile: "off",
 			hideSliderAtLimit: 0,
